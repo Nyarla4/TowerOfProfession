fcf0ecdb85463094579784d680661331888c0489
Nyarla4 <31647681+Nyarla4@users.noreply.github.com>
2026-02-19 23:18:45 +0900

    폴더 이름 변경 및 미사용 코드 삭제

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -6,9 +6,7 @@ public class PlayerInput : MonoBehaviour
 {
     /// <summary> WASD, 방향키 </summary>
     public Vector2 MoveInputMap { get; private set; }
-    /// <summary> 마우스 위치 </summary>
-    public Vector2 PointInputMap { get; private set; }
-    
+        
     private bool isPause;
     /// <summary> ESC </summary>
     public bool IsPause {
@@ -23,24 +21,7 @@ public class PlayerInput : MonoBehaviour
     }
     /// <summary> ESC 이벤트 </summary>
     public Action<bool> OnPause;
-
-    private bool isSprint;
-    /// <summary> Shift </summary>
-    public bool IsSprint
-    {
-        get { return isSprint; }
-        private set
-        {
-            if (value != isSprint)
-            {
-                OnSprint?.Invoke(value);
-            }
-            isSprint = value;
-        }
-    }
-    /// <summary> Shift 이벤트 </summary>
-    public Action<bool> OnSprint;
-    
+        
     private bool isInteract;
     /// <summary> Shift </summary>
     public bool IsInteract
@@ -55,7 +36,7 @@ public class PlayerInput : MonoBehaviour
             isInteract = value;
         }
     }
-    /// <summary> Shift 이벤트 </summary>
+    /// <summary> Interact 이벤트 </summary>
     public Action<bool> OnInteract;
 
     public void OnMove(InputAction.CallbackContext context)
@@ -71,27 +52,9 @@ public class PlayerInput : MonoBehaviour
         MoveInputMap = value;
     }
 
-    public void Point(InputAction.CallbackContext context)
-    {
-        if (Time.timeScale == 0)
-        {
-            PointInputMap = Vector2.zero;
-            return;
-        }
-
-        Vector2 value = context.ReadValue<Vector2>();
-
-        PointInputMap = value;
-    }
-
     public void Pause(InputAction.CallbackContext context)
     {
         IsPause = context.ReadValueAsButton();
-    }
-
-    public void Sprint(InputAction.CallbackContext context)
-    {
-        IsSprint = context.ReadValueAsButton();
     }
 
     public void Interact(InputAction.CallbackContext context)
